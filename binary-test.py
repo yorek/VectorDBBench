47119809d75b7ca6e8fc98dafb305e9b14b825c5
Davide Mauri <damauri@microsoft.com>
2024-01-26 10:52:20 -0800

    updated for installing pip

---
+++ binary-test.py
@@ -0,0 +1,32 @@
+import pyodbc
+import struct
+import binascii
+
+uid = "benchmark"
+pwd = "B3nch_mark"
+connection_string = (
+    r"DRIVER={ODBC Driver 18 for SQL Server};"
+    r"SERVER=LAPTOP-DM-2.local;"
+    r"DATABASE=vectordb;"
+    f"UID={uid};PWD={pwd};TrustServerCertificate=Yes"
+)
+
+b = bytearray()
+b.append(169)
+b.append(170)
+
+items:float = [100, 2000, 1, 0, -1, 0.3, 200]
+
+b += bytearray(struct.pack("i", len(items)))
+
+b.append(0)
+b.append(0)
+
+for i in range(len(items)):
+    b += bytearray(struct.pack("f", items[i]))
+    
+
+cnxn = pyodbc.connect(connection_string, autocommit=True)
+crsr = cnxn.cursor()
+crsr.execute("insert into [benchmark].[vector_768] (id, [vector]) values (1, ?)", b)
+crsr.close()
