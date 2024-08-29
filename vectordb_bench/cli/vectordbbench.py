a08209382977bd8adb65c2ff88cbac95c814d281
Josh Innis (Warner Marketing Incorporated) <v-innisjosh@microsoft.com>
2024-08-29 10:42:08 -0700

    Add MSSQL CLI

--- vectordb_bench/cli/vectordbbench.py
+++ vectordb_bench/cli/vectordbbench.py
@@ -7,7 +7,7 @@ from ..backend.clients.weaviate_cloud.cli import Weaviate
 from ..backend.clients.zilliz_cloud.cli import ZillizAutoIndex
 from ..backend.clients.milvus.cli import MilvusAutoIndex
 from ..backend.clients.aws_opensearch.cli import AWSOpenSearch
-
+from ..backend.clients.mssql.cli import MSSQL
 
 from .cli import cli
 
@@ -21,7 +21,7 @@ cli.add_command(Test)
 cli.add_command(ZillizAutoIndex)
 cli.add_command(MilvusAutoIndex)
 cli.add_command(AWSOpenSearch)
-
+cli.add_command(MSSQL)
 
 if __name__ == "__main__":
     cli()
